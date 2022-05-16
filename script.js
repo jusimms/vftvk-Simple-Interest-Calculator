7a14c0a937e3d26765affc68407dd779dda51255
Julia Simms <juliasimms10@gmail.com>
2022-05-16 19:37:40 +0000

    inner html text and form validation

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function compute()
     var interest = principal * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years);
 
-    document.getElementById("result").innerHTML = `If you deposit ${principal} at an interest rate of ${rate}. You will recieve an amount of ${interest}, in the year ${year}`;
+    document.getElementById("result").innerHTML = `If you deposit <span style="background-color: yellow">${principal}</span> at an interest rate of <span style="background-color: yellow">${rate}</span>. You will recieve an amount of <span style="background-color: yellow">${interest}</span>, in the year <span style="background-color: yellow">${year}</span>`;
 }
         
 function updateRate() 
@@ -15,3 +15,6 @@ function updateRate()
     document.getElementById("rate_val").innerText=rateval;
 }
 
+function principalValidation() {
+    if ()
+}
